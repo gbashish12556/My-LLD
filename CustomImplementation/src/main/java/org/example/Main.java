3ed9db2af8d94944a433cfe9f22881741ec31ae4
Ashish Gupta <ashish.gupta@nobroker.in>
2023-03-10 16:43:28 +0530

    NA

--- CustomImplementation/src/main/java/org/example/Main.java
+++ CustomImplementation/src/main/java/org/example/Main.java
@@ -3,11 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        HashMapImpl<Integer,String> hashMap = new HashMapImpl<>(20);
-        hashMap.put(21,"21");
-        hashMap.put(1,"01");
-
-        System.out.println(hashMap.get(1));
-
     }
+
 }
